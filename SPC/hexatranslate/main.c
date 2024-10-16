ddc4ae315271ffbfd9ef91e67be58b84a4d7d8cd
Thillonoz <emilivarsson92@gmail.com>
2024-10-16 10:28:05 +0200

    Made Hexa converter

--- SPC/hexatranslate/main.c
+++ SPC/hexatranslate/main.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
 
-int main()
+int main(void)
 {
+    int input = 0;
+    int validInput = 0;
+    while (1)
+    {
+        printf("Input your number to convert to hexadecimal: ");
+        validInput = scanf("%d", &input);
 
+        if (validInput == 1)
+        {
+            printf("%x\n", input);
+            break;
+        }
+        else
+        {
+            printf("Wrong input, please input an integer.\n");
+            while (getchar() != '\n')
+                ;
+        }
+    }
     return 0;
 }
