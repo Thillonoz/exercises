6b1cef11cbcc02da7187b2c0c6d4a4faa421d483
Thillonoz <emilivarsson92@gmail.com>
2024-10-21 10:59:42 +0200

    Fixing exercise10

--- SPC/exercise10/main.c
+++ SPC/exercise10/main.c
@@ -4,11 +4,17 @@ int main()
 {
     int input = 0;
     int valid = 1;
-    int binArray[20];
+    int binArray[9];
+    for (int i = 0; i < 10; i++)
+    {
+        binArray[i] = 0;
+    }
+
     printf("Input a number 0-255: \n");
     scanf("%d", &input);
 
-    if (input < 0 || input > 255) {
+    if (input < 0 || input > 255)
+    {
         printf("Input a number 0-255: \n");
         scanf("%d", &input);
     }
@@ -18,7 +24,7 @@ int main()
         binArray[i] = input % 2;
         input = input / 2;
     }
-    for (int i = 8; i >= 0; i--)
+    for (int i = 7; i >= 0; i--)
     {
         printf("%d", binArray[i]);
     }
