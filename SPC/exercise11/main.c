f51ee855bb5138ac80a3e28e26a3f664d1bb01aa
Thillonoz <emilivarsson92@gmail.com>
2024-10-21 21:47:31 +0200

    More fixes for 10, 11, 13, 14

--- SPC/exercise11/main.c
+++ SPC/exercise11/main.c
@@ -31,7 +31,6 @@ int main()
       }
     }
   }
-
-  putchar('/n');
+  printf("\n");
   return 0;
 }
--- SPC/exercise14/main.c
+++ SPC/exercise14/main.c
@@ -22,16 +22,19 @@ int primeCheck(int num)
 int main()
 {
     int num = 0;
-
-    printf("Input a number 0 - 1000 ");
-    scanf("%d", &num);
-
-    while (primeCheck(num) != 1)
+    while (num < 1 || num > 1000)
     {
-        printf("Given number %d is not a prime number. Please try again.\n", num);
+        printf("Input a number 0 - 10000 ");
         scanf("%d", &num);
-        primeCheck(num);
     }
-    printf("Congratulations, %d is a prime number!.\n", num);
+
+    if (primeCheck(num) == 1)
+    {
+        printf("Given number %d is a prime number.\n", num);
+    }
+    else
+    {
+        printf("Given number %d is not a prime number.\n", num);
+    }
     return 0;
 }
