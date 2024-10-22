ef4e6589bbf6597a0502d33fbcb60f0895243db2
Thillonoz <emilivarsson92@gmail.com>
2024-10-22 18:55:56 +0200

    Fixing exercise18

--- SPC/exercise18/main.c
+++ SPC/exercise18/main.c
@@ -7,17 +7,17 @@ int main()
 {
     int arr[3][3];
     int count = sizeof(arr) / sizeof(arr[0][0]);
-    int j = 0;
-    int i = 0;
+    int j = 0, i = 0, l = 0, k = 0;
     for (i = 0; i < SIZE_OF_MATRIX; i++)
     {
         printf("Please input every element of this 2D array:\n");
         scanf("%d", &arr[j][i]);
     }
     i = 0;
-    for (int l = 0; l < SIZE_OF_ARRAY; l++, i++)
+    printf("Your table looks like: \n");
+    for (l = 0; l < SIZE_OF_ARRAY; l++, i++)
     {
-        for (int k = 0; k < SIZE_OF_ARRAY; k++)
+        for (k = 0; k < SIZE_OF_ARRAY; k++)
         {
             printf("%d\t", arr[l][k]);
         }
