05cfeb64d46cbd3296ac4357b22c5f6b7b6306f1
Thillonoz <emilivarsson92@gmail.com>
2024-10-23 07:40:03 +0200

    Added exercise19

--- SPC/exercise19/main.c
+++ SPC/exercise19/main.c
@@ -1,5 +1,17 @@
+/**
+ * @file main.c
+ * @author Emil Ivarsson (emilivarsson92@gmail.com)
+ * @brief
+ * @version 0.1
+ * @date 2024-10-23
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 
 int main()
 {
@@ -7,7 +19,28 @@ int main()
 
     printf("Input text: \n");
     fgets(arr, 50, stdin);
+    int inputLength = strlen(arr);
+    printf("\nOutput: \n");
+    for (int i = 0; i < inputLength - 1; i++)
+    {
+        if (isspace(arr[i]) && isspace(arr[i + 1]))
+        {
+            while (isspace(arr[i + 1]))
+            {
+                i++;
+                continue;
+            }
+            printf("\n");
+            continue;
+        }
+        else if (isspace(arr[i]) && !isspace(arr[i - 1]))
+        {
+            printf("\n");
+            continue;
+        }
+        printf("%c", arr[i]);
+    }
+    printf("\n");
 
-    printf("%d \n", arr[14]);
     return 0;
 }
