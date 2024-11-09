768f3ba0fdae3ba723b7beb4be5b6b6b84d6e1f0
Thillonoz <emilivarsson92@gmail.com>
2024-11-09 06:18:16 +0100

    more work

--- SPC/assignments/c/1/main.c
+++ SPC/assignments/c/1/main.c
@@ -13,43 +13,39 @@
 #include <stdint.h>
 
 #define FORMAT "YYMMDD-NNNN" // Expected format
-#define PERS_NUM_LEN 10      // The number of digits in a swedish personal number
-#define INPUT_LEN 11         // Number of digits + '-'
-#define YEAR_MIN 47          // 1947 is the start year for personal numbers
+#define ERROR "Invalid input, try again (%s)\n"
+#define PERS_NUM_LEN 10 // The number of digits in a swedish personal number
+#define INPUT_LEN 11    // Number of digits + '-'
 #define MONTH_MIN 1
 #define MONTH_MAX 12
 #define DAY_MIN 1
 #define DAY_MAX 31
 
-void clearBuffer(void);
-bool checkPersonalNumber(const int *personNumber);
+bool checkPersonalNumber(const int8_t *personNumber);
 
 int main(void)
 {
     char input[INPUT_LEN] = {0};
     int8_t year = 0, month = 0, day = 0, serial = 0;
-    int parsedInput[INPUT_LEN] = {0};
+    int8_t parsedInput[INPUT_LEN] = {0};
     bool result = false;
 
-    // Prompt the user for input
     (void)printf("Please enter your personal number (%s) to check if it's valid: \n", FORMAT);
 
-    // Check if scanf succeeds in reading the input
+    // Limits the input and checks if scanf succeeds in reading the input
     if (scanf("%11s", input) != 1)
     {
-        (void)printf("Input error. Please try again.\n");
-        clearBuffer();
-        return 1;
+        (void)printf(ERROR, FORMAT);
+        return 0;
     }
 
     // Use sscanf to parse the input
-    if (sscanf(input, "%2hhd%02hhd%02hhd-%4hhd", &year, &month, &day, &serial) == 4)
+    if (sscanf(input, "%2hhd%2hhd%2hhd-%4hhd", &year, &month, &day, &serial) == 4)
     {
         // Check date range and display an error if invalid
-        if (year < YEAR_MIN || year < MONTH_MIN || month > MONTH_MAX || day < DAY_MIN || day > DAY_MAX)
+        if (month < MONTH_MIN || month > MONTH_MAX || day < DAY_MIN || day > DAY_MAX)
         {
-            (void)printf("Invalid date. Please try again. (%s)\n", FORMAT);
-            clearBuffer();
+            (void)printf(ERROR, FORMAT);
             return 0;
         }
 
@@ -61,26 +57,18 @@ int main(void)
 
         // Validate the personal number and display result
         result = checkPersonalNumber(parsedInput);
-        (void)printf("It's %s match!\n", result == true ? "a" : "not a");
-        return 0;
+        (void)printf("The number %s valid!\n", result ? "is" : "is not");
     }
     else
     {
-        printf("Incorrect input, try again (%s)\n", FORMAT);
+        (void)printf(ERROR, FORMAT);
     }
-}
-// Clears the input buffer
-void clearBuffer(void)
-{
-    int8_t c;
-    while ((c = getchar()) != '\n' && c != EOF)
-        ;
+    return 0;
 }
 // Validates Swedish personal number check digit
-bool checkPersonalNumber(const int *personNumber)
+bool checkPersonalNumber(const int8_t *personNumber)
 {
     int8_t checkDigit = 0, product = 0;
-    printf("%ld", sizeof(checkDigit));
     for (size_t i = 0, j = 0; i < PERS_NUM_LEN - 1; i++, j++)
     {
         if (i == 6) // Skip the '-'
