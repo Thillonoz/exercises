5195e6ccbb098b6b2cdc43da8060c5d238cfd717
Thillonoz <emilivarsson92@gmail.com>
2024-11-17 22:08:23 +0100

    some testing on vg

--- SPC/assignments/c/1/vg/test.c
+++ SPC/assignments/c/1/vg/test.c
@@ -1,55 +1,94 @@
 /**
- * @file test.c
+ * @file main.c
  * @author Emil Ivarsson (emilivarsson92@gmail.com)
- * @brief This program does CRC-15 checking to a message
+ * @brief A program to calculate the CRC of given data and check and validate the checksum
  * @version 0.1
  * @date 2024-11-17
  *
  * @copyright Copyright (c) 2024
  *
  */
-
-#include <stdio.h>
 #include <stdint.h>
+#include <stdio.h>
+#include <stdbool.h>
 
 #define POLYNOMIAL 0xC599
-#define CRC_WIDTH 15
 #define N 14
 #define BIT_SIZE 8
 #define BIT_SHIFT 1
-#define CRC_INIT 0x0000 // Initial CRC value
 
-uint16_t crc15(uint8_t *data, size_t length)
-{
-    uint16_t crc = CRC_INIT; // Initialize CRC with the specified initial value
-    uint16_t sum = 0;
-
-    for (uint8_t i = 0; i < length; i++)
-    {
-        crc ^= (data[i] << (CRC_WIDTH - BIT_SIZE)); // Align the byte with CRC's high bit
-
-        for (uint8_t j = 0; j < BIT_SIZE; j++)
-        {
-            if (crc & (BIT_SHIFT << (CRC_WIDTH - 1))) // If the leftmost bit is set
-            {
-                sum += (crc << BIT_SHIFT) ^ POLYNOMIAL; // Shift left and XOR with polynomial
-            }
-            else
-            {
-                crc <<= BIT_SHIFT; // Just shift left
-            }
-        }
-    }
-    return sum;
-}
+uint16_t compute_crc(const uint8_t *message, size_t length);
+bool verify_crc(const uint8_t *message, size_t length, uint16_t received_crc);
 
 int main(void)
 {
-    uint8_t data[] = {'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd', '!'}; // Example data
-    size_t length = sizeof(data) / sizeof(data[0]);
+    uint8_t message[N] = {'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd', '!', 0, 0}; // Example message
+    size_t length = sizeof(message) / sizeof(message[0]);
 
-    uint16_t sum = crc15(data, length);
-    (void)printf("CRC-15 checksum: 0x%X\n", sum);
+    // Compute CRC-15 for the message
+    uint16_t crc = compute_crc(message, length);
+    printf("CRC-15 checksum: 0x%X\n", crc);
 
+    // Verify the CRC
+    bool is_valid = verify_crc(message, length, crc);
+    printf("CRC verification: %s\n", is_valid ? "Passed" : "Failed");
+
+    printf("Computed CRC %X\n", crc);
+    crc = ~crc;
+    printf("Not computed CRC %X\n", crc);
+    printf("Message 13 %X\n", message[12]);
+    message[12] = 0xF4;
+    printf("Message 13 %X\n", message[12]);
+    printf("Message 14 %X\n", message[13]);
+    message[13] = 0x1C;
+    printf("Message 14 %X\n", message[13]);
+    printf("Whole message %X\n", &message);
+    uint16_t checkNumber = 0;
+    for (int i = 0; i < N; i++)
+    {
+        checkNumber += message[i];
+    }
+    uint16_t mod = checkNumber % POLYNOMIAL;
+    printf("Modulus %hhu\n", mod);
+    printf("It's %s\n", checkNumber % POLYNOMIAL == 0 ? "Valid" : "Not valid");
     return 0;
+}
+
+// Function to compute CRC-15 checksum
+uint16_t compute_crc(const uint8_t *message, size_t length)
+{
+    uint16_t crc = 0; // Initial CRC value (0 or all 1s is common)
+
+    for (uint8_t i = 0; i < length; i++)
+    {
+        uint8_t byte = message[i];
+
+        // Process each bit in the byte, from LSB to MSB
+        for (uint8_t j = 0; j < BIT_SIZE; j++)
+        {
+            uint8_t bit = byte & 1; // Extract the LSB
+            byte >>= BIT_SHIFT;     // Shift to the next bit in the byte
+
+            // Update the CRC by shifting left by 1 and XORing with the polynomial
+            if ((crc & (1 << N)) ^ (bit << N))
+            {
+                crc = (crc << BIT_SHIFT) ^ POLYNOMIAL;
+            }
+            else
+            {
+                crc <<= BIT_SHIFT;
+            }
+        }
+    }
+
+    // Mask to ensure CRC is within the 15-bit range
+    return crc & 0xFFF;
+}
+
+// Function to verify CRC on a message with appended checksum
+bool verify_crc(const uint8_t *message, size_t length, uint16_t received_crc)
+{
+    uint16_t computed_crc = compute_crc(message, length);
+
+    return (computed_crc == received_crc);
 }
